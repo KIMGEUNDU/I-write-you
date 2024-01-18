a1ccb5ec07dae23e3ac82518534e7fd0dfac9ffb
HYHYJ <jjangdaram@naver.com>
2024-01-18 18:59:18 +0900

    markup: 공통 css width값 설정
    공통 css width값 설정

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,12 +2,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
+import { commonWidth } from '@/style/mq';
 
 export default function RootLayout() {
   return (
     <>
       <Header />
-      <main>
+      <main css={commonWidth}>
         <Outlet />
       </main>
       <Footer />
