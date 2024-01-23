5b54d1f80e5716e54225aac7fe1553bde4e6ba30
rustandbone <dlehdgh14@naver.com>
2024-01-23 14:09:52 +0900

    markup: 편지 정보 입력 및 작성 페이지 마크업 및 부분 스타일

--- src/pages/WriteInfo.tsx
+++ src/pages/WriteInfo.tsx
@@ -1,9 +1,77 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 export default function WriteInfo() {
   return (
     <>
-      <h1>편지에 대한 정보 쓰기 페이지</h1>
+      <section
+        css={{
+          padding: '1rem',
+        }}
+      >
+        <h1
+          css={css({
+            textAlign: 'center',
+            paddingBottom: '1rem',
+          })}
+        >
+          누구에게 편지를 쓰실 건가요?
+        </h1>
+
+        <article
+          css={css({
+            width: '10rem',
+            margin: '0 auto',
+          })}
+        >
+          <div>
+            <input type="checkbox" id="member" />
+            <label htmlFor="member">친구</label>
+          </div>
+          <select
+            name="memberFriends"
+            id="memberFriends"
+            css={css({
+              width: '100%',
+            })}
+          >
+            <option value="" disabled selected>
+              ---
+            </option>
+            <option value="김건주">김건주</option>
+            <option value="장효윤">장효윤</option>
+            <option value="정소이">정소이</option>
+          </select>
+        </article>
+        <article
+          css={css({
+            width: '10rem',
+            margin: '0 auto',
+          })}
+        >
+          <div>
+            <input type="checkbox" id="nonMember" />
+            <label htmlFor="nonMember">비회원</label>
+          </div>
+          <input
+            type="text"
+            placeholder="편지를 받을 친구의 이름을 입력해주세요."
+            css={css({
+              width: '100%',
+            })}
+          />
+        </article>
+
+        <button
+          type="button"
+          css={css({
+            margin: '1rem auto 0',
+            display: 'block',
+          })}
+        >
+          선택
+        </button>
+      </section>
     </>
   );
 }
--- src/pages/WriteLetter.tsx
+++ src/pages/WriteLetter.tsx
@@ -1,9 +1,82 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 export default function WriteLetter() {
   return (
     <>
-      <h1>편지 쓰기 페이지</h1>
+      <section
+        css={{
+          padding: '1rem',
+        }}
+      >
+        <article>
+          <h3
+            css={css({
+              textAlign: 'center',
+            })}
+          >
+            편지지를 선택해주세요.
+          </h3>
+          <ul
+            css={css({
+              display: 'flex',
+              gap: '1rem',
+              justifyContent: 'center',
+            })}
+          >
+            <li>편지지1</li>
+            <li>편지지2</li>
+            <li>편지지3</li>
+            <li>편지지4</li>
+          </ul>
+        </article>
+        <hr />
+        <textarea
+          name=""
+          id=""
+          placeholder="편지를 작성해주세요"
+          css={css({
+            width: '100%',
+            height: '70vh',
+            background: 'transparent',
+            border: 'gray 1px solid',
+            fontSize: '1.2rem',
+            fontFamily: 'Pretendard',
+            lineHeight: '1.8',
+            padding: '1rem',
+            boxSizing: 'border-box',
+          })}
+        ></textarea>
+        <button type="button">사진 첨부하기</button>
+
+        <div>
+          <label htmlFor="photo" className="">
+            사진 등록
+          </label>
+          <input
+            type="file"
+            accept="image/jpg,image/png,image/jpeg,image/webp,image/avif"
+            name="photo"
+            id="photo"
+          />
+          <div>
+            <img src="/vite.svg" alt="photo" />
+          </div>
+        </div>
+
+        <article>
+          <h3>
+            비회원의 경우, 편지를 열람하기 위한 코드가 필요합니다. 4자리의
+            코드를 입력해주세요.
+          </h3>
+          <p>
+            편지 링크와 코드 4자리를 전달하시면, 코드 입력 후 편지를 읽을 수
+            있습니다.
+          </p>
+          <input type="password" name="" id="" minLength={4} maxLength={4} />
+          <input type="password" name="" id="" minLength={4} maxLength={4} />
+        </article>
+      </section>
     </>
   );
 }
