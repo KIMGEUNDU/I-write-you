f430ac4b6037ab8a1925b2fa6b0181bcc9c657b3
rustandbone <dlehdgh14@naver.com>
2024-01-29 16:22:09 +0900

    style: 편지 쓰기 페이지 스타일 추가

--- src/pages/WriteLetter.tsx
+++ src/pages/WriteLetter.tsx
@@ -7,12 +7,37 @@ export default function WriteLetter() {
       <section
         css={{
           padding: '1rem',
+          background: "url('bg/illust7.jpg')",
+          backgroundPositionX: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          minHeight: '100vh',
         }}
       >
+        <textarea
+          name=""
+          id=""
+          placeholder="편지를 작성해주세요"
+          css={css({
+            width: '100%',
+            height: '100vh',
+            background: 'rgba(255, 255, 255, 0.7)',
+            border: 'gray 1px solid',
+            fontSize: '1.2rem',
+            fontFamily: 'Pretendard',
+            lineHeight: '1.8',
+            padding: '1rem',
+            boxSizing: 'border-box',
+            outline: 'none',
+            resize: 'none',
+          })}
+        ></textarea>
+        <hr />
         <article>
           <h3
             css={css({
               textAlign: 'center',
+              marginBottom: '0.5rem',
             })}
           >
             편지지를 선택해주세요.
@@ -24,58 +49,79 @@ export default function WriteLetter() {
               justifyContent: 'center',
             })}
           >
-            <li>편지지1</li>
-            <li>편지지2</li>
-            <li>편지지3</li>
-            <li>편지지4</li>
+            <li>
+              <button
+                type="button"
+                css={css({ cursor: 'pointer', outline: 'none' })}
+              >
+                <img
+                  src="/bg/illust1.jpg"
+                  alt=""
+                  css={css({
+                    width: '100px',
+                    objectFit: 'cover',
+                  })}
+                />
+              </button>
+            </li>
+            <li>
+              {' '}
+              <button
+                type="button"
+                css={css({ cursor: 'pointer', outline: 'none' })}
+              >
+                <img
+                  src="/bg/illust4.jpg"
+                  alt=""
+                  css={css({
+                    width: '100px',
+                    objectFit: 'cover',
+                  })}
+                />
+              </button>
+            </li>
+            <li>
+              {' '}
+              <button
+                type="button"
+                css={css({ cursor: 'pointer', outline: 'none' })}
+              >
+                <img
+                  src="/bg/illust2.jpg"
+                  alt=""
+                  css={css({
+                    width: '100px',
+                    objectFit: 'cover',
+                  })}
+                />
+              </button>
+            </li>
+            <li>
+              {' '}
+              <button
+                type="button"
+                css={css({ cursor: 'pointer', outline: 'none' })}
+              >
+                <img
+                  src="/bg/illust9.jpg"
+                  alt=""
+                  css={css({
+                    width: '100px',
+                    objectFit: 'cover',
+                  })}
+                />
+              </button>
+            </li>
           </ul>
         </article>
-        <hr />
-        <textarea
-          name=""
-          id=""
-          placeholder="편지를 작성해주세요"
+        <button
           css={css({
-            width: '100%',
-            height: '70vh',
-            background: 'transparent',
-            border: 'gray 1px solid',
-            fontSize: '1.2rem',
-            fontFamily: 'Pretendard',
-            lineHeight: '1.8',
-            padding: '1rem',
-            boxSizing: 'border-box',
+            display: 'block',
+            margin: '0 auto',
           })}
-        ></textarea>
-        <button type="button">사진 첨부하기</button>
-
-        <div>
-          <label htmlFor="photo" className="">
-            사진 등록
-          </label>
-          <input
-            type="file"
-            accept="image/jpg,image/png,image/jpeg,image/webp,image/avif"
-            name="photo"
-            id="photo"
-          />
-          <div>
-            <img src="/vite.svg" alt="photo" />
-          </div>
-        </div>
-
-        <article>
-          <h3>
-            비회원의 경우, 편지를 열람하기 위한 코드가 필요합니다. 4자리의
-            코드를 입력해주세요.
-          </h3>
-          <p>
-            편지 링크와 코드 4자리를 전달하시면, 코드 입력 후 편지를 읽을 수
-            있습니다.
-          </p>
-          <input type="password" name="" id="" minLength={4} maxLength={4} />
-          <input type="password" name="" id="" minLength={4} maxLength={4} />
-        </article>
+        >
+          발송
+        </button>
       </section>
     </>
   );
