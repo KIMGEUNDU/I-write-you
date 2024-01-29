64f0ac9051377081d6bc02e2c1c724b6ba2f4aca
KIMGEUNDU <vlkoklv@naver.com>
2024-01-29 17:14:42 +0900

    init: 메인페이지
    related to: #7

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,84 @@
 /** @jsxImportSource @emotion/react */
+import { informationState } from '@/recoil/atom/useOpen';
+import { Common } from '@/style/Common';
+import { mq } from '@/style/mq';
 import { css } from '@emotion/react';
-
-const hotpink = css({
-  color: 'hotpink',
-  background: ['red', 'linear-gradient(#e66465, #9198e5)'],
-  '&:hover': css({ color: 'whitesmoke' }),
-});
+import { useRecoilState } from 'recoil';
 
 export default function Main() {
-  const main = document.querySelector('main');
-  main?.setAttribute('style', 'background: #2D3A6F');
+  const [open, setOpen] = useRecoilState(informationState);
 
   return (
-    <>
-      <div
-        css={css({
-          color: 'green',
-          '&:hover': hotpink,
-        })}
-      >
-        첫화면
+    <div css={div}>
+      <p css={title}>
+        <span>I WRITE</span>
+        <span css={you}>YOU</span>
+      </p>
+      <div css={background}>
+        <span css={subTitle}>Save Memories</span>
       </div>
-      <div css={hotpink}>핫핑크</div>
-    </>
+      <button type="button" css={information} onClick={() => setOpen(!open)}>
+        ?
+      </button>
+    </div>
   );
 }
+
+const div = css({
+  background: '#ed94a1',
+  position: 'relative',
+  overflow: 'hidden',
+  width: '100%',
+  height: '100%',
+});
+
+const title = mq({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  zIndex: 1,
+  fontSize: ['25vw', '20vw', '20vw', '200px'],
+  color: Common.colors.lightYellow,
+});
+
+const background = mq({
+  width: '100%',
+  height: '80vh',
+  background: `${Common.colors.mint} url("/sliceHotel.png") no-repeat 50% 90%`,
+  backgroundSize: 'auto 75%',
+  transform: 'translateY(30%)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: ['center', 'center', 'flex-end', 'flex-end'],
+});
+
+const you = css({
+  position: 'absolute',
+  bottom: '-60%',
+  left: '15px',
+});
+
+const subTitle = mq({
+  position: 'absolute',
+  top: '90px',
+  right: '-80px',
+  fontSize: '30px',
+  transform: 'rotate(90deg)',
+  color: Common.colors.lightYellow,
+});
+
+const information = css({
+  position: 'absolute',
+  top: '120px',
+  right: '0px',
+  border: `none`,
+  backgroundColor: Common.colors.darkPurple,
+  color: 'white',
+  borderRadius: '50%',
+  fontSize: '20px',
+  width: '30px',
+  height: '30px',
+  cursor: 'pointer',
+});
