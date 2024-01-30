60d682b199fddd2cf22e06adb3ad1ca5099c4ee0
KIMGEUNDU <vlkoklv@naver.com>
2024-01-30 16:05:19 +0900

    feat: 모달 추가 및 체크인 버튼 생성
    ref: #7

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,22 +4,36 @@ import { Common } from '@/style/Common';
 import { mq } from '@/style/mq';
 import { css } from '@emotion/react';
 import { useRecoilState } from 'recoil';
+import { TbHandClick } from 'react-icons/tb';
+import { clickAnimation } from '@/util/clickAnimation';
+import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
   const [open, setOpen] = useRecoilState(informationState);
+  const navigate = useNavigate();
 
   return (
     <div css={div}>
       <p css={title}>
         <span>I WRITE</span>
-        <span css={you}>YOU</span>
+        <span css={you}>
+          YOU
+          <button
+            type="button"
+            css={information}
+            onClick={() => setOpen(!open)}
+          >
+            ?
+          </button>
+        </span>
       </p>
       <div css={background}>
         <span css={subTitle}>Save Memories</span>
+        <button type="button" css={clickBtn} onClick={() => navigate('/login')}>
+          <TbHandClick css={click} />
+          check-in
+        </button>
       </div>
-      <button type="button" css={information} onClick={() => setOpen(!open)}>
-        ?
-      </button>
     </div>
   );
 }
@@ -37,27 +51,68 @@ const title = mq({
   top: 0,
   left: 0,
   display: 'flex',
-  flexDirection: 'column',
+  alignItems: 'center',
   zIndex: 1,
   fontSize: ['25vw', '20vw', '20vw', '200px'],
   color: Common.colors.lightYellow,
 });
 
-const background = mq({
-  width: '100%',
-  height: '80vh',
-  background: `${Common.colors.mint} url("/hotel.png") no-repeat 50% 90%`,
-  backgroundSize: 'auto 75%',
-  transform: 'translateY(30%)',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: ['center', 'center', 'flex-end', 'flex-end'],
+const information = css({
+  border: `none`,
+  backgroundColor: Common.colors.darkPurple,
+  color: 'white',
+  borderRadius: '50%',
+  fontSize: '20px',
+  width: '30px',
+  height: '30px',
+  cursor: 'pointer',
 });
 
 const you = css({
   position: 'absolute',
   bottom: '-60%',
   left: '15px',
+});
+
+const background = css({
+  position: 'relative',
+  width: '100%',
+  height: '80vh',
+  background: `${Common.colors.mint} url("/hotel.png") no-repeat center 85%`,
+  backgroundSize: 'auto 70%',
+  transform: 'translateY(30%)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-end',
+});
+
+const clickBtn = css({
+  position: 'absolute',
+  bottom: '50px',
+  right: '50%',
+  transform: 'translateX(50%) translateY(7%)',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'transparent',
+  cursor: 'pointer',
+  border: 'none',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'InkLipquid',
+  fontSize: '30px',
+  color: Common.colors.darkPurple,
+});
+
+const click = css({
+  color: Common.colors.darkPurple,
+  width: '70px',
+  height: '50px',
+  animation: `${clickAnimation} 1s linear infinite`,
+  '&:hover': css({
+    transform: 'scale(110%)',
+  }),
 });
 
 const subTitle = mq({
@@ -67,18 +122,4 @@ const subTitle = mq({
   fontSize: '30px',
   transform: 'rotate(90deg)',
   color: Common.colors.lightYellow,
-});
-
-const information = css({
-  position: 'absolute',
-  top: '120px',
-  right: '0px',
-  border: `none`,
-  backgroundColor: Common.colors.darkPurple,
-  color: 'white',
-  borderRadius: '50%',
-  fontSize: '20px',
-  width: '30px',
-  height: '30px',
-  cursor: 'pointer',
 });
