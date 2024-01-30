8778692877cd9e47878bcef2a6e61243cf1348b7
KIMGEUNDU <vlkoklv@naver.com>
2024-01-30 16:04:38 +0900

    feat: 클릭 애니메이션
    ref: #7

---
+++ src/util/clickAnimation.ts
@@ -0,0 +1,13 @@
+import { keyframes } from '@emotion/react';
+
+export const clickAnimation = keyframes`
+    0%{
+      transform: scale(100%);
+    }
+
+
+    100%{
+      transform: scale(110%);
+    }
+
+`;
