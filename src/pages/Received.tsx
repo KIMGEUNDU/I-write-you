c4fdee739d86018224b73061fd637f506445c12d
uniS2 <s2oie1010@gmail.com>
2024-02-16 14:02:09 +0900

    feat: 받는편지함 페이지 기능 구현
    - '편지보내기' 기능 완료 후 Supabase 정보 Read -> 화면 렌더링
    - 보낸이 텍스트 :hover 시 전체 제공 기능
    - 방금 받은 편지 CSS 애니메이션 효과(호텔 키) 구현 (5분 이내)
    - MenuButton 렌더링
    
    related to #19

--- src/pages/Received.tsx
+++ src/pages/Received.tsx
@@ -1,39 +1,104 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import { Common } from '@/style/Common';
-import { mq } from '@/style/mq';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { supabase } from '@/client';
+import { css, keyframes } from '@emotion/react';
 
-const data = Array(15).fill(1);
+import MenuButton from '@/components/MenuButton';
+import { debounce } from '@/util/debounce';
+import { letterSentRecent } from '@/util/letterSentRecent';
+import { mq } from '@/style/mq';
+import { Common } from '@/style/Common';
+
+type ReceivedItemProp = {
+  id: number;
+  receiver: string;
+  created_at: string;
+};
 
 export default function Received() {
+  const [receivedData, setReceivedData] = useState<ReceivedItemProp[] | null>(
+    null
+  );
+  const [emptyData, setEmptyData] = useState<Array<number> | null>([]);
+  const [hover, setHover] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchReceived = async () => {
+      try {
+        const { data } = await supabase
+          .from('letter')
+          .select('id, created_at, receiver');
+        // filter: 로그인 - sender 일치할 경우만
+        setReceivedData(data!.reverse());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchReceived();
+  }, []);
+
+  useEffect(() => {
+    if (receivedData) {
+      const emptyData = Array(16 - receivedData!.length)
+        .fill(0)
+        .map((v, i) => v + i);
+      setEmptyData(emptyData);
+    }
+  }, [receivedData, setReceivedData]);
+
   return (
     <section css={background}>
       <h1 css={srOnly}>받는 편지함</h1>
-      {/* 받는 편지: props 통해 img */}
       <div css={gridLayout}>
-        <div css={letterBoxLayout} key={'a'}>
-          <dl css={namePlate}>
-            <dt css={srOnly}>받는 사람</dt>
-            <dd css={font}>라이온</dd>
-          </dl>
-          <div css={namePlateLine} aria-hidden />
-          <div css={letterBox} aria-label="받는 편지함">
-            <img src="/gift.png" alt="선물" />
-          </div>
-        </div>
+        {/* 받는 편지 */}
+        {receivedData &&
+          receivedData.map((item: ReceivedItemProp) => {
+            const isRecent = letterSentRecent(item.created_at);
+            const animationStyle = isRecent
+              ? css`
+                  animation: ${twinkleEffect} 2s alternate infinite;
+                `
+              : '';
+
+            return (
+              <div css={letterBoxLayout} key={item.id}>
+                <div
+                  css={nameAnimationLayout}
+                  onMouseEnter={() => setHover(item.id)}
+                  onMouseLeave={debounce(() => setHover(null))}
+                >
+                  <dl css={namePlate}>
+                    <dt css={srOnly}>받는 사람</dt>
+                    <dd css={name}>{item.receiver}</dd>
+                  </dl>
+                  {hover == item.id && (
+                    <div css={hoverName}>{item.receiver}</div>
+                  )}
+                </div>
+                <div css={namePlateLine} aria-hidden />
+                <Link
+                  css={letterBox}
+                  to={`/receivedRead/${item.id}`}
+                  aria-label="받는 편지함"
+                >
+                  <img css={animationStyle} src="/gift.png" alt="선물" />
+                </Link>
+              </div>
+            );
+          })}
         {/* 편지 없을 때 */}
-        {data.map((_, index) => (
-          <div css={letterBoxLayout} key={index}>
-            <dl css={namePlate}>
-              <dt css={srOnly}>받는 사람</dt>
-              <dd css={font} />
-            </dl>
-            <div css={namePlateLine} aria-hidden />
-            <div css={letterBox} aria-label="빈 편지함" />
-          </div>
-        ))}
+        {emptyData!.length > 0 &&
+          emptyData!.map((_, index) => (
+            <div css={letterBoxLayout} key={index}>
+              <div css={namePlate} />
+              <div css={namePlateLine} aria-hidden />
+              <div css={letterBox} aria-label="빈 편지함" />
+            </div>
+          ))}
       </div>
       <img src="/mailMan.png" alt="배달원" css={frontMan} />
+      <MenuButton sent />
     </section>
   );
 }
@@ -60,13 +125,18 @@ const background = css({
   overflow: 'hidden',
 });
 
-const font = css({
+const name = css({
   position: 'relative',
-  top: '50%',
-  transform: 'translateY(-50%)',
+  top: '55%',
+  left: '50%',
+  transform: 'translateX(-50%) translateY(-50%)',
+  width: '70%',
   fontSize: '1.875rem',
   letterSpacing: '-0.1094rem',
   textAlign: 'center',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
 });
 
 const gridLayout = mq({
@@ -86,10 +156,44 @@ const letterBoxLayout = css({
   alignItems: 'center',
 });
 
+const nameAnimationLayout = css({
+  position: 'relative',
+});
+
 const namePlate = css({
   width: ' 7.875rem',
   height: '3.0625rem',
   background: `url('/namePlate.png') no-repeat center / cover`,
+});
+
+const hoverName = css({
+  position: 'fixed',
+  top: '3rem',
+  width: 'max-contents',
+  padding: '0.125rem 0.75rem',
+  border: `1px solid ${Common.colors.lightPink}`,
+  borderRadius: '0.375rem',
+  zIndex: 1,
+  background: `${Common.colors.lightMint}`,
+  color: `${Common.colors.lightPink}`,
+  fontSize: '1.5rem',
+  letterSpacing: '-0.1094rem',
+  textAlign: 'center',
+  transition: 'top 1s ease-in',
+
+  ':hover': {
+    top: '1.5rem',
+    animationDuration: '3s',
+    animationName: keyframes`
+    0% {
+      opacity: 0; 
+    }
+
+    100% {
+      opacity: 1;
+    }
+  `,
+  },
 });
 
 const namePlateLine = css({
@@ -115,9 +219,24 @@ const letterBox = css({
 });
 
 const frontMan = css({
-  position: 'absolute',
+  position: 'fixed',
   left: '50%',
   bottom: '-5rem',
   width: '20.5625rem',
   transform: 'translateX(-50%)',
 });
+
+const twinkleEffect = keyframes`
+ 0% {
+    filter: drop-shadow(0 0 3px rgb(255, 255, 255, 0.4));
+  }
+
+  50% {
+    filter: drop-shadow(0 0 18px rgb(255, 255, 255, 0.8));
+  }
+
+  100% {
+    filter: drop-shadow(0 0 3px rgb(255, 255, 255, 0.4));
+  }
+
+`;
