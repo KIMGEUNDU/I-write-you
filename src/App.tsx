41aa530f32a121a807471c6a6b441fae2f15e2cc
rustandbone <dlehdgh14@naver.com>
2024-02-16 19:02:14 +0900

    feat: 로그인 세션 체크 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,40 @@ import { RecoilRoot } from 'recoil';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './style/GlobalStyle';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { supabase } from './supabaseClient';
+import { Session } from '@supabase/supabase-js';
+import Main from './pages/Main';
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <RecoilRoot>
       <HelmetProvider>
         <Suspense>
           <GlobalStyle />
-          <RouterProvider router={router} />
+          {session ? (
+            <RouterProvider router={router} />
+          ) : (
+            <>
+              <Main />
+            </>
+          )}
         </Suspense>
       </HelmetProvider>
     </RecoilRoot>
