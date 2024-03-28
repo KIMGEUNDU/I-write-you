f40981c32eca4e2c4ede26916288d0a2dae0902c
uniS2 <s2oie1010@gmail.com>
2024-03-28 21:10:09 +0900

    feat: 호텔개시 페이지 읽은 편지 `read` 컬럼 update
    related to: #30

--- src/pages/ReceivedRead.tsx
+++ src/pages/ReceivedRead.tsx
@@ -1,6 +1,30 @@
 /** @jsxImportSource @emotion/react */
 
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { supabase } from '@/supabaseClient';
+
 export default function ReceivedRead() {
+  const paramsId = useParams();
+
+  useEffect(() => {
+    const updateLetterRead = async () => {
+      // 이미 true인 경우 update X
+      const { data, error } = await supabase
+        .from('letter')
+        .update({ read: true })
+        .eq('id', paramsId);
+
+      if (error) {
+        console.error('Error updating letter read: ', error);
+      }
+
+      return data;
+    };
+
+    updateLetterRead();
+  }, [paramsId]);
+
   return (
     <>
       <h1>받은 편지 읽는 페이지</h1>
